087da900e96798fc9d7d4325446752a0d0bb9ca3
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-04-01 05:55:26 +0300

    Update Demorbidentor.cs

--- 1.5/Assemblies/Demorbidentor.cs
+++ 1.5/Assemblies/Demorbidentor.cs
@@ -340,5 +340,23 @@ namespace Demorbidentor
 			return text;
 		}
 	}
-  
+
+	public class IngestionOutcomeDoer_OffsetDemorbidentor : IngestionOutcomeDoer
+	{
+		public float offset;
+		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+		{
+			GeneUtility.OffsetDemorbidentor(pawn, offset * (float)ingestedCount);
+		}
+		public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+		{
+			if (ModsConfig.BiotechActive)
+			{
+				string text = ((offset >= 0f) ? "+" : string.Empty);
+				yield return new StatDrawEntry(StatCategoryDefOf.BasicsNonPawnImportant, "Demorbidentor".Translate().CapitalizeFirst(), text + Mathf.RoundToInt(offset * 100f), "DemorbidentorDesc".Translate(), 1000);
+			}
+		}
+	}
+ 
+	
 }
