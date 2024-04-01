8f99b33bb7a68d7a2fb06bf40ad820d24690f95a
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-04-01 05:49:17 +0300

    Update Demorbidentor.cs

--- 1.5/Assemblies/Demorbidentor.cs
+++ 1.5/Assemblies/Demorbidentor.cs
@@ -10,6 +10,39 @@ using Verse.AI;
 namespace Demorbidentor
 {
 
+  public class CompAbilityEffect_DemorbidentorCost : CompAbilityEffect
+	{
+		public new CompProperties_AbilityDemorbidentorCost Props => (CompProperties_AbilityDemorbidentorCost)props;
+		
 
+		public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+			base.Apply(target, dest);
+			GeneUtility.OffsetDemorbidentor(parent.pawn, 0f - Props.demorbidentorCost);
+		}
+
+		public override bool GizmoDisabled(out string reason)
+		{
+			Gene_Demorbidentor gene_Demorbidentor = parent.pawn.genes?.GetFirstGeneOfType<Gene_Demorbidentor>();
+			if (gene_Demorbidentor == null)
+			{
+				reason = "NoDemorbidentorGene".Translate(parent.pawn);
+				return true;
+			}
+			if (gene_Demorbidentor.Value < Props.demorbidentorCost)
+			{
+				reason = "FewDemorbidentor".Translate(parent.pawn);
+				return true;
+			}
+			float num = Props.demorbidentorCost;
+			if (Props.demorbidentorCost > float.Epsilon && num > gene_Demorbidentor.Value)
+			{
+				reason = "FewDemorbidentor".Translate(parent.pawn);
+				return true;
+			}
+			reason = null;
+			return false;
+		}
+	}
   
 }
