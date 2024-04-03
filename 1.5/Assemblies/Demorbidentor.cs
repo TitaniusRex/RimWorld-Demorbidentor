44a0af619c4d42787260c96bddce90c96b92d1eb
TitaniusRex <127874235+TitaniusRex@users.noreply.github.com>
2024-04-03 10:14:32 +0300

    Update Demorbidentor.cs

--- 1.5/Assemblies/Demorbidentor.cs
+++ 1.5/Assemblies/Demorbidentor.cs
@@ -581,5 +581,31 @@ namespace Demorbidentor
 		}
 	}
 	
-	
+	//++++++++++++++++
+
+	internal class CompAddHediff : CompAbilityEffect
+	{
+		public new CompProperties_AddHediff Props => (CompProperties_AddHediff)props;
+
+		public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+		{
+			base.Apply(target, dest);
+			if (Props.applyToCaster)
+			{
+				parent.pawn.health.AddHediff(Props.hediffDef);
+			}
+			if (!Props.applyToRadius)
+			{
+				return;
+			}
+			foreach (Pawn item in parent.pawn.Map.mapPawns.AllPawnsSpawned)
+			{
+				if (item.Spawned && item.Position.InHorDistOf(target.Cell, parent.def.EffectRadius))
+				{
+					item.health.AddHediff(Props.hediffDef);
+				}
+			}
+		}
+	}
+ 
 }
